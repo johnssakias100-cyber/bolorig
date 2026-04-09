5678aef3fcc5d56b09a17eebb444ff30bc22cc09
johnssakias100-cyber <johnssakias100@gmail.com>
2026-04-10 00:44:52 +0300

    move to root

---
+++ app/src/main/java/io/github/johnssakias100_cyber/bolorig/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.johnssakias100_cyber.bolorig;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
